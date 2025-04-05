eb99b2c9052d333e739667e56d5984a9c4676ffd
FlavioNovati <f.novati10@gmail.com>
2025-04-05 17:10:52 +0200

    Update

--- Assets/Scripts/HouseBuilder_Tool/Editor/HouseBuilderModule_Editor.cs
+++ Assets/Scripts/HouseBuilder_Tool/Editor/HouseBuilderModule_Editor.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
 using UnityEditor;
 using UnityEditor.SceneManagement;
+using System.Runtime.InteropServices;
 
 namespace Tool.ModularHouseBuilder
 {
     [CustomEditor(typeof(HouseBuilderModule))]
     public class HouseBuilderModule_Editor : Editor
     {
+        const float HANDLE_SIZE = 0.25f;
+        const float HANDLE_DISTANCE = 0.15f;
+
         private HouseBuilderModule _module;
         private MeshFilter _meshFilter;
 
         private bool _useBoundsForCollisions = true;
+        private int _nearestHandle;
+        private Vector2 previousMousePos;
 
         void OnEnable()
         {
@@ -27,14 +33,101 @@ namespace Tool.ModularHouseBuilder
 
         public void OnSceneGUI()
         {
-            return;
+            if (PrefabStageUtility.GetCurrentPrefabStage() == null)
+                return;
+
+            //Draw Orign
+            Handles.SphereHandleCap(-1, Vector3.zero, Quaternion.identity, 0.01f, EventType.Repaint);
+
+            //Draw Object Extension
+            Vector3 objectExtension = _module.ModuleData.Extension;
+            Vector3 offset = _module.ModuleData.CenterOffset;
+
+            //Draw Handles Cap
+            Vector3 yHandlePos = new Vector3(offset.x, objectExtension.y, offset.z);
+            Vector3 yHandleOffset = new Vector3(0f, HANDLE_DISTANCE, 0f);
+
+            Vector3 xHandlePos = new Vector3(objectExtension.x / 2f, objectExtension.y / 2f, offset.z);
+            Vector3 xHandleOffset = new Vector3(HANDLE_DISTANCE, 0f, 0f);
+
+            Vector3 zHandlePos = new Vector3(offset.x, objectExtension.y / 2f, objectExtension.z / 2f);
+            Vector3 zHandleOffset = new Vector3(0f, 0f, HANDLE_DISTANCE);
+
+
+            int hoverIndex = -1;
+            if (Event.current.type == EventType.Repaint)
+            {
+                hoverIndex = HandleUtility.nearestControl;
+
+                Handles.color = hoverIndex == 11 ? Color.magenta : Color.green;
+                DrawHandleCap(11, yHandlePos + yHandleOffset, Vector3.up, HANDLE_SIZE, EventType.Repaint);
+
+                Handles.color = hoverIndex == 12 ? Color.magenta : Color.red;
+                DrawHandleCap(12, xHandlePos + xHandleOffset, Vector3.right, HANDLE_SIZE, EventType.Repaint);
+
+                Handles.color = hoverIndex == 13 ? Color.magenta : Color.blue;
+                DrawHandleCap(13, zHandlePos + zHandleOffset, Vector3.back, HANDLE_SIZE, EventType.Repaint);
+            }
+
+            if (Event.current.type == EventType.Layout)
+            {
+                DrawHandleCap(11, yHandlePos + yHandleOffset, Vector3.up, HANDLE_SIZE, EventType.Layout);
+                DrawHandleCap(12, xHandlePos + xHandleOffset, Vector3.right, HANDLE_SIZE, EventType.Layout);
+                DrawHandleCap(13, zHandlePos + zHandleOffset, Vector3.back, HANDLE_SIZE, EventType.Layout);
+            }
+            
+
+            if (Event.current.type == EventType.MouseDown && Event.current.button == 0)
+            {
+                _nearestHandle = HandleUtility.nearestControl;
+                previousMousePos = Event.current.mousePosition;
+
+                Undo.RegisterCompleteObjectUndo(_module.ModuleData, "Module Modified");
+                Undo.FlushUndoRecordObjects();
+            }
+
+            if (Event.current.type == EventType.MouseDrag)
+            {
+                if(_nearestHandle == 11)
+                {
+                    float move = HandleUtility.CalcLineTranslation(previousMousePos, Event.current.mousePosition, yHandlePos + yHandleOffset, Vector3.up);
+                    _module.ModuleData.Extension.y += move;
+                    _module.ModuleData.Extension.y = Mathf.Clamp(_module.ModuleData.Extension.y, 0, float.MaxValue);
+                }
+                if(_nearestHandle == 12)
+                {
+                    float move = HandleUtility.CalcLineTranslation(previousMousePos, Event.current.mousePosition, xHandlePos + xHandleOffset, Vector3.right);
+                    _module.ModuleData.Extension.x += move;
+                    _module.ModuleData.Extension.x = Mathf.Clamp(_module.ModuleData.Extension.x, 0, float.MaxValue);
+                }
+                if(_nearestHandle == 13)
+                {
+                    float move = HandleUtility.CalcLineTranslation(previousMousePos, Event.current.mousePosition, zHandlePos + zHandleOffset, Vector3.forward);
+                    _module.ModuleData.Extension.z += move;
+                    _module.ModuleData.Extension.z = Mathf.Clamp(_module.ModuleData.Extension.z, 0, float.MaxValue);
+                }
+
+                previousMousePos = Event.current.mousePosition;
+                SceneView.RepaintAll();
+            }
+
+            if (Event.current.type == EventType.MouseUp && Event.current.button == 0)
+            {
+                _nearestHandle = -1;
+                previousMousePos = Vector2.zero;
+            }
         }
+
+        private void DrawHandleCap(int controlID, Vector3 startPos, Vector3 direction, float size, EventType eventType)
+            => Handles.ArrowHandleCap(controlID, startPos, Quaternion.LookRotation(direction), size, eventType);
 
         public override void OnInspectorGUI()
         {
             _useBoundsForCollisions = GUILayout.Toggle(_useBoundsForCollisions, "Use Bounds for Collision", GUILayout.ExpandWidth(true));
             if (GUILayout.Button("Update Collider", GUILayout.ExpandWidth(true)))
                 UpdateCollider();
+            if (GUILayout.Button("Reset Extension", GUILayout.ExpandWidth(true)))
+                ResetExtension();
         }
 
         private void DuringSceneGUI(SceneView view)
@@ -79,7 +172,7 @@ namespace Tool.ModularHouseBuilder
             Vector3 meshRotation = _meshFilter.gameObject.transform.eulerAngles;
             
             //Get Collider Size
-            Vector3 colliderSize = _meshFilter.sharedMesh.bounds.extents * 2f;
+            Vector3 colliderSize = _module.ModuleData.Extension;
             colliderSize.x *= meshScale.x;
             colliderSize.z *= meshScale.z;
             colliderSize.y *= meshScale.y;
@@ -88,6 +181,17 @@ namespace Tool.ModularHouseBuilder
             //Set Collider Size and Offset
             BoxCollider boxCollider = _module.gameObject.GetComponent<BoxCollider>();
             boxCollider.size = colliderSize;
+        }
+
+        private void ResetExtension()
+        {
+            //Get Params
+            Vector3 meshScale = _meshFilter.gameObject.transform.lossyScale;
+            Vector3 meshRotation = _meshFilter.gameObject.transform.eulerAngles;
+
+            //Get Collider Size
+            Vector3 extendsSize = _meshFilter.sharedMesh.bounds.extents * 2f;
+            //TODO: Fix
         }
     }
 }
--- Assets/Scripts/HouseBuilder_Tool/HouseBuilderModule.cs
+++ Assets/Scripts/HouseBuilder_Tool/HouseBuilderModule.cs
@@ -1,8 +1,4 @@
 using UnityEngine;
-using UnityEditor.SceneManagement;
-using static UnityEditor.PlayerSettings;
-
-
 
 #if UNITY_EDITOR
 using UnityEditor;
@@ -13,87 +9,16 @@ namespace Tool.ModularHouseBuilder
     public class HouseBuilderModule : MonoBehaviour
     {
         public ModuleData ModuleData;
+        public ModuleType ModuleType => ModuleData.ModuleType;
 
-#if UNITY_EDITOR
-        const float HANDLE_DISTANCE = 0.15f;
-
+        #if UNITY_EDITOR
         private void OnDrawGizmos()
         {
-            if (PrefabStageUtility.GetCurrentPrefabStage() == null)
-                return;
-
-            Handles.zTest = UnityEngine.Rendering.CompareFunction.LessEqual;
-
-            //Draw Orign
-            Handles.SphereHandleCap(-1, Vector3.zero, Quaternion.identity, 0.01f, EventType.Repaint);
-
-            //Draw Object Extension
+            //Draw Extension
             Vector3 objectExtension = ModuleData.Extension;
             Vector3 offset = ModuleData.CenterOffset;
             Handles.DrawWireCube(offset, objectExtension);
-
-            Vector3 yHandlePos = new Vector3(offset.x, objectExtension.y + HANDLE_DISTANCE, offset.z);
-            Vector3 xHandlePos = new Vector3(objectExtension.x / 2f + HANDLE_DISTANCE, objectExtension.y / 2f, offset.z);
-            Vector3 zHandlePos = new Vector3(offset.x, objectExtension.y / 2f, -objectExtension.z / 2f - HANDLE_DISTANCE);
-
-            //Draw Buttons
-            //Y
-            DrawHandleCap(0, yHandlePos, Vector3.up, 0.5f, Color.green);
-            //X
-            DrawHandleCap(1, xHandlePos, Vector3.right, 0.5f, Color.red);
-            //Z
-            DrawHandleCap(2, zHandlePos, Vector3.forward, 0.5f, Color.blue);
-            
-            int controlID = GUIUtility.GetControlID(FocusType.Passive);
-            Debug.Log(controlID);
-
-            switch(Event.current.GetTypeForControl(controlID))
-            {
-                case EventType.MouseMove:
-                    break;
-                
-                case EventType.MouseUp:
-                    break;
-
-                case EventType.MouseDown:
-                    if(controlID == HandleUtility.nearestControl && Event.current.button == 0)
-                    {
-                        Debug.Log(controlID);
-                        GUIUtility.hotControl = controlID;
-                        Event.current.Use();
-                    }
-                    break;
-            }
         }
-
-        private void DrawHandleCap(int controllID, Vector3 startPos, Vector3 direction, float size, Color color)
-        {
-            Handles.color = color;
-            Handles.ArrowHandleCap(controllID, startPos, Quaternion.LookRotation(direction), size, EventType.Repaint);
-            HandleUtility.AddControl(controllID, 0.25f);
-        }
-
-        /*
-         * case EventType.MouseDown:
-                // Log the nearest control ID if the click is near or directly above
-                // the solid disc handle.
-                if (controlID == HandleUtility.nearestControl && evt.button == 0)
-                {
-                    Debug.Log($"The nearest control is {controlID}.");
-
-                    GUIUtility.hotControl = controlID;
-                    evt.Use();
-                }
-                break;
-
-            case EventType.MouseUp:
-                if (GUIUtility.hotControl == controlID && evt.button == 0)
-                {
-                    GUIUtility.hotControl = 0;
-                    evt.Use();
-                }
-                break
-        */
-#endif
+        #endif
     }
 }
